556eae68026996265597f24381a9bd5e10067077
grorge <g97116@yahoo.com>
2019-02-13 22:53:41 +0800

    many thing

---
+++ Offline_judge_V2.0/bin/Debug/README.md
@@ -0,0 +1,26 @@
+此judge有4種模式
+請在judge執行程式目錄下放入比賽資料夾,裡面須包含一個index.txt檔案和名稱為inp,judge,score,out,pro,special的資料夾
+index.txt第一行為n代表題目數量,接下來有n行,每行分別為type is_strict ans pro time special
+tpye 為題目模式分別有 normal hack debug special
+is_strict 為是否嚴格比對請放入 0或1
+ans 為題目在ans資料夾的答案檔案名稱
+pro 為pi,i為第幾題的數字
+time 為題目時間限制
+special 除了normal模式以外都要輸入代表在special資料夾special judge的名稱(在debug模式下special 為edit number)
+並且在輸入完題目後輸入m代表有多少使用者並在接下來m行打上使用者名稱
+
+請將各題目的答案放在judge資料夾並且命名為pi,i為第幾題的數字
+請使用者將上傳程式傳入inp資料夾檔名格式為"檔案名稱_pi.cpp"i為第幾題的數字
+並且會將使用者的成績輸出在score資料夾裡
+
+normal 為一般的輸入測資測試答案是否正確,因此不需要放東西到ans資料夾,但還是要填名字
+hack 為測試使用者輸入的測資是否能使程式執行不正確,程式會將使用者的文件傳入special judge裡面請自行判斷範圍並輸出應該輸出的內容,並比對輸出內容跟正確答案的差別,而ans資料夾的程式為正確範例的程式,如果使用者輸入不符合範圍的測資請回傳re
+debug 將一份錯誤程式碼給使用者debug並傳上正確的,會將debug的程式做一般的normal模式處理但會確認edit number,並且在ans資料夾放入正確原始code
+special 直接將使用者的程式輸出給special judge並由special judge 的回傳值來表示結果,因此也不需要放入ans資料
+
+0 = "Oh! CE! Maybe you need to see your code again"
+1 = "Wow!That is AC!!!You are so smart!"
+2 = "TLE! Your program run time is to more";
+3 = "WA!!Your program runs result is not same as anser!!!(Be careful!!!Otherwise you will see me again) *\(O_O)/*";
+4 = "RE!!Maybe some bug in your code but it can be compile";
+5 = "SE!!WTF! Please tell manager the problem";
--- solar_V2/main.cpp
+++ solar_V2/main.cpp
@@ -25,23 +25,18 @@ struct cycle{
 };
 int n;
 int s[10005][10005]={};
-inline long double change_cos(long double a){
-    return cos(a * M_PI / 180);
-}
-inline long double change_sin(long double a){
-    return sin(a * M_PI / 180);
-}
 inline long double f_sin_fi(double long del,double long sin_om,long double cos_H){
-    return (change_cos(del) * sin_om)/cos_H;
+    return (cos(del) * sin_om)/cos_H;
 }
 inline long double f_sin_H(long double cos_phi,long double cos_del,long double cos_om,long double del,long double phi){
-    return (cos_phi * cos_del * cos_om) + (change_sin(del) * change_sin(phi));
+    return (cos_phi * cos_del * cos_om) + (sin(del) * sin(phi));
 }
 inline long double f_sin_H0(long double del,long double phi){
-    return change_cos(del - phi);
+    return cos(del - phi);
 }
-inline long double f_del(long double X){
-    return 0.006918 - 0.399912 * change_cos(X) + 0.070257 * change_sin(X) - 0.006758 * change_cos(2 * X) + 0.000908 * change_sin(2 * X);
+inline long double f_del(long double X,long double N){
+    //return -23.45 * cos(360.0/365.0 * (N + 10));
+    return 0.006918 - 0.399912 * cos(X) + 0.070257 * sin(X) - 0.006758 * cos(2 * X) + 0.000908 * sin(2 * X);
 }
 inline long double f_X(long double N){
     return (2 * M_PI * (N - 1))/365;
@@ -50,7 +45,7 @@ inline long double f_om(long double lam,long double E){
     return lam + 15 * E;
 }
 inline long double f_E(long double X){
-    return (0.0172 + 0.4281 * change_cos(X) - 7.3515 * change_sin(X) - 3.3495 * change_cos(2 * X) - 9.3619 * change_sin(2 * X))/60;
+    return (0.0172 + 0.4281 * cos(X) - 7.3515 * sin(X) - 3.3495 * cos(2 * X) - 9.3619 * sin(2 * X))/60;
 }
 inline long double f_I(long double sin_H,long double sin_H0){
     return (0.42 * sin_H) + (((2.92 - sin_H0) / (2 * sin_H0)) * pow(sin_H,2)) - (((2.92 - sin_H0) / (4 * pow(sin_H0,2))) * pow(sin_H,3));
@@ -68,44 +63,48 @@ inline long double f_IbBR(long double IDN,long double cos_si){
     return IDN * cos_si;
 }
 inline long double f_IsBR(long double I,long double ISH,long double IoH,long double cos_si,long double sin_H,long double B){
-    return ISH * (((I-ISH) / (IoH)) * ((cos_si) / (sin_H)) + (1 + ((I-ISH) / (IoH))) * ((1 + change_cos(B)) / (2)));
+    return ISH * (((I-ISH) / (IoH)) * ((cos_si) / (sin_H)) + (1 + ((I-ISH) / (IoH))) * ((1 + cos(B)) / (2)));
 }
 inline long double f_IoH(long double sin_H,long double N){
-    return ISC * (1 + 0.033 * change_cos((N - 2) * ((2 * M_PI) / (365)))) * sin_H;
+    return ISC * (1 + 0.033 * cos((N - 2) * ((2 * M_PI) / (365)))) * sin_H;
 }
 inline long double f_cos_si(long double del,long double om,long double B,long double R,long double phi){
-    return (change_sin(phi) * change_cos(B) - change_cos(phi) * change_sin(B) * change_cos(R)) * change_sin(del) + (change_cos(phi) * change_cos(B) + change_sin(phi) * change_sin(B) * change_cos(R)) * change_cos(del) * change_cos(om) + change_sin(B) * change_sin(R) * change_cos(del) *change_sin(om);
+    return (sin(phi) * cos(B) - cos(phi) * sin(B) * cos(R)) * sin(del) + (cos(phi) * cos(B) + sin(phi) * sin(B) * cos(R)) * cos(del) * cos(om) + sin(B) * sin(R) * cos(del) *sin(om);
 }
 inline long double f_IrBR(long double I,long double ro,long double B){
-    return (ro * I) * ((1 - change_cos(B)) / (2));
+    return (ro * I) * ((1 - cos(B)) / (2));
 }
 inline long double f_ro(long double NS){
     return 0.55;//new concrete Typical albedo (https://en.wikipedia.org/wiki/Albedo)
 //    return 0.2 * (1 - NS) + 0.7 * NS;
 }
-inline long double get_IBR(long double N,long double R,long double B,long double phi,long double lam){
+inline long double get_IBR(long double N,long double R,long double B,long double phi,long double lam,long double om){
+    om = ((om) * M_PI)/180;
     long double X = f_X(N);
-    long double E = f_E(X);
-    long double del = f_del(X),om = f_om(lam,E);
-    long double sin_H = f_sin_H(change_cos(phi),change_cos(del),change_cos(om),del,phi),sin_H0 = f_sin_H0(del,phi);
+//    long double E = f_E(X);
+    long double del = f_del(X,N)/*,om = f_om(lam,E)*/;
+    long double sin_H = f_sin_H(cos(phi),cos(del),cos(om),del,phi),sin_H0 = f_sin_H0(del,phi);
     long double I = f_I(sin_H,sin_H0);
     long double IDN = f_IDN(I,sin_H),cos_si = f_cos_si(del,om,B,R,phi),IoH = f_IoH(sin_H,N);
     long double ISH = f_ISH(I,IDN,sin_H),ro = f_ro(0);
     long double IbBR = f_IbBR(IDN,cos_si),IsBR = f_IsBR(I,ISH,IoH,cos_si,sin_H,B),IrBR = f_IrBR(I,ro,B);
+    long double cos_H = cos(asin(sin_H));
+    long double sin_fi = f_sin_fi(del,sin(om),cos_H);
+//    cout << N << ' ' << om/15+12 << ' ' << asin (sin_fi) * 180.0 / M_PI << ' ' << asin (sin_H) * 180.0 / M_PI << endl;
+//    cout << sin_fi << endl;
+    cout << sin_fi << ',';
     return f_IBR(IsBR,IbBR,IrBR);
 }
 inline bool init(long double* N,long double* R,long double* B,long double* phi,long double* lam){
     string address;
     cout << "input the file name:";
     cin >> address;
-    cout << "input Azimuth Angle of PV Module:";
-    cin >> *R;
-    cout << "input Tilt Angle of PV Module:";
-    cin >> *B;
     cout << "input Latitude:";
     cin >> *phi;
+    *phi = ((*phi) * M_PI)/180;
     cout << "input Longitude:";
     cin >> *lam;
+    *lam = ((*lam) * M_PI)/180;
     fstream file;
 //    file.open(address,'r');
     if(file){
@@ -121,11 +120,15 @@ inline bool init(long double* N,long double* R,long double* B,long double* phi,l
     return 1;
 }
 void special_point(){
-    for(long double i = 0; i < 360; i++){
-        for(long double q = 0; q < 360; q++){
-            cout << "方位:" << i << ' ' << "傾斜:" << q << ' '<< "能量:" << get_IBR(365.0,i,q,25.0,121.0) << endl;
-        }
+    for(int i = -75; i < 75;i += 15){
+//        113,174,267,357
+        get_IBR(357,0,0,((25) * M_PI)/180,((121) * M_PI)/180,i);
     }
+//    for(long double i = 0; i < 360; i++){
+//        for(long double q = 0; q < 360; q++){
+//            cout << "方位:" << i << ' ' << "傾斜:" << q << ' '<< "能量:" << get_IBR(365.0,i,q,25.0,121.0) << endl;
+//        }
+//    }
 }
 int main(){
     IOS
