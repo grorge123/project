62a82ade4ab45c3cce44e978939c75846ae34c25
grorge <g97116@yahoo.com>
2019-05-26 12:50:38 +0800

    update badcontral

--- unity test/Assets/control.cs
+++ unity test/Assets/control.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class control : MonoBehaviour
 {
     static public Vector3 people_position;
+    public int hp = 100;
     // Start is called before the first frame update
     void Start()
     {
+        hp = 100;
+        gameObject.transform.position = new Vector3(-6, -1, 0);
         people_position = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (hp == 0)
+        {
+            Instantiate(gameObject, new Vector3(-6, -1, 0), gameObject.gameObject.transform.rotation);
+            Destroy(gameObject);
+        }
         if (Input.GetKey(KeyCode.W))
         {
             gameObject.transform.position += new Vector3(0, 0.1f, 0);
@@ -31,5 +39,13 @@ public class control : MonoBehaviour
             gameObject.transform.position += new Vector3(0.1f, 0, 0);
         }
         people_position = gameObject.transform.position;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Bullet")
+        {
+            hp -= 10;
+            Destroy(col.gameObject);
+        }
     }
 }
--- unity test/Assets/gunmove.cs
+++ unity test/Assets/gunmove.cs
@@ -20,13 +20,16 @@ public class gunmove : MonoBehaviour
         const float PI = 3.14159265359F;
         float tan = (gameObject.transform.position.y - Input.mousePosition.y) / (gameObject.transform.position.x - Input.mousePosition.x);
         tan = Mathf.Atan(tan) * 180 / PI;
-        gameObject.transform.rotation =  Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y, tan);
+        //gameObject.transform.rotation =  Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y, tan);
+        float angleZ = Mathf.Atan2(Input.mousePosition.y, Input.mousePosition.x) * Mathf.Rad2Deg;
+        gameObject.transform.eulerAngles = new Vector3(0, 0, angleZ);
+        Vector2 mpos = RectTransformUtility.WorldToScreenPoint(Camera.main, Input.mousePosition);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             print("fire");
             Vector3 pos = gameObject.transform.position;
             pos.x += 2;
-            print(tan);
+            print(string.Format("{0} {1}", mpos.x, mpos.y));
             Instantiate(Bullet, pos, Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y, tan ));
         }
     }
